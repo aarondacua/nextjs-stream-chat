68be9bf33ce40a603ba1757d4c7e7a6e7f42c4c0
aarondacua <nadacua78@gmail.com>
2024-05-25 18:55:55 +0800

    stream setup

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,49 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
+import { Wind } from "lucide-react";
+import { StreamChat } from "stream-chat";
+import {
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+
+const userId = "user_2gxEnMrqqoBpNqi5pUWmRvalVxi";
+
+const chatClient = StreamChat.getInstance(
+  process.env.NEXT_PUBLIC_STREAM_API_KEY!
+);
+
+chatClient.connectUser(
+  {
+    id: userId,
+    name: "Onin",
+  },
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8yZ3hFbk1ycXFvQnBOcWk1cFVXbVJ2YWxWeGkifQ.S14vH-dY1DCJeS75yg2OjhrNwWft14zzOHGGfNaBmm4"
+);
+
+const channel = chatClient.channel("messaging", "channel_1", {
+  name: "Channel #1",
+  members: [userId],
+});
 
 export default function ChatPage() {
   return (
     <div>
-      <h1>Chat</h1>
-      <UserButton afterSignOutUrl="/" />
+      <Chat client={chatClient}>
+        <Channel channel={channel}>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
     </div>
   );
 }
